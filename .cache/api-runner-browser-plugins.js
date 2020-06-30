fdfb6accfc681cb903d63fb3273bc309f773b34b
pcollinsTech <phil@pcollins.tech>
2020-06-30 06:57:57 +0100

    Remove Cache tweaked gitignore

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"start_url":"/","icon":"src/assets/images/logo_new.png"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
